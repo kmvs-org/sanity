d32e44dd5453ec27eaea634f8c02febec08cfca6
Rithvik <x394@protonmail.com>
2025-08-11 16:24:45 +0530

    added slug field to page

--- schemaTypes/page.js
+++ schemaTypes/page.js
@@ -1,5 +1,6 @@
 import { HiDocument } from 'react-icons/hi2'
 import title from './ui/title'
+import slug from './ui/slug'
 import content from './ui/content'
 
 export default {
@@ -9,6 +10,7 @@ export default {
     icon: HiDocument,
     fields: [
         title,
+        slug({ type: '' }),
         content,
     ],
 }
